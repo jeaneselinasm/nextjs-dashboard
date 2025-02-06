587f7d795f5c54f0cabb70a7e0141db24436bbcb
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-02-06 12:49:09 +0900

    [fix]:can't use /login

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async generateStaticParams() {
+    return [{ slug: "login" }]; // ✅ Forces Next.js to generate the login page
+  },
   
 };
 

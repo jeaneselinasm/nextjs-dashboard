726142d56e54abe615175ac1dfd4abf65b17435c
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-02-06 12:05:29 +0900

    [fix]:auth.ts result user not found

--- auth.ts
+++ auth.ts
@@ -9,15 +9,21 @@ import { sql } from '@vercel/postgres';
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const result = await sql<{ rows: User[] }>`SELECT * FROM users WHERE email=${email}`;
-      return result.rows.length > 0 ? result.rows[0] : undefined;
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
+  try {
+    const result: { rows: User[] } = await sql`SELECT * FROM users WHERE email=${email}`;
+
+    // Ensure result is defined before accessing `rows`
+    if (!result || !result.rows || result.rows.length === 0) {
+      return undefined;
     }
+
+    return result.rows[0]; // Return the first user if available
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
-  
+}
+
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
